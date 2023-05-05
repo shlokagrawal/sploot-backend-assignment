e061ffff81758a18edcfeae410542f696b480fb5
Shlok Agrawal <shlokagrawal188@gmail.com>
2023-05-05 07:16:16 +0530

    modify app.js

--- app.js
+++ app.js
@@ -127,10 +127,7 @@ function verifyToken(req, res, next) {
     const bearer = bearerHeader.split(" ");
     const token = bearer[1];
     // console.log(token);
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized access' });
-    }
-
+    
     jwt.verify(token, process.env.JWT_SECRET_KEY , (err, decoded) => {
         if (err) {
             return res.status(401).json({ error: 'Invalid token' });
